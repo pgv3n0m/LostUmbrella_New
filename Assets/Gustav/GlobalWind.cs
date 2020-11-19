3c2aa01f454441f403c1d38aaa4737db17154857
gussvens <gussvens@student.chalmers.se>
2020-11-19 12:19:53 +0100

    Adds GlocalWind script

---
+++ Assets/Gustav/GlobalWind.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalWind : MonoBehaviour
+{
+    public Umbrella_Movement1 umbrella;
+    public AudioSource windSFX;
+
+    public int id;
+    public Vector2 windDirection;
+    public Vector2 direction;
+    public float magnitude;
+
+    // Update is called once per frame
+    void Update() {
+        umbrella.removeForce(id);
+        
+        // TODO: Randomize wind changes, keep in mind to change it gradually since this is Update()
+
+        direction = windDirection - Vector2.zero;
+
+        umbrella.addForce(id, magnitude, direction);
+    }
+}
