778f0a27672ddccb2e02aecc6229d49907e617c8
gussvens <gussvens@student.chalmers.se>
2020-11-23 13:54:07 +0100

    Adds new sprites and camera movement

---
+++ Assets/Gustav/CameraPlayerFollowScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayerFollowScript : MonoBehaviour
+{
+    public Transform player;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(player.position.x, this.transform.position.y, this.transform.position.z);
+    }
+}
